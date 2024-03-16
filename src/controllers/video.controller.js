b01b1e6ad858803786cdeb7023d72bbb0813c0c7
shailesh1416 <shaileshyadav1416@gmail.com>
2024-03-16 12:56:41 +0530

    added deletevideo, updatevideo, togglepublishvideo controllers

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -17,11 +17,9 @@ const getAllVideos = asyncHandler(async (req, res) => {
     const sorttype = sortType?parseInt(sortType):1
 
     let allVideos;
-    // if (userId){
-    //     allVideos = await Video.find()
-    // }else{
-        allVideos = await Video.find(userId?{_id:userId}:{}).skip(0).limit(limit).sort({[sortby]:sorttype})
-    // }
+
+    allVideos = await Video.find(userId?{_id:userId}:{}).skip(0).limit(limit).sort({[sortby]:sorttype})
+   
     if (!allVideos) {
         throw new ApiError(500, "Something went wrong while retriving the video")
     }
@@ -45,6 +43,7 @@ const publishAVideo = asyncHandler(async (req, res) => {
         const videoLocalPath = req.files?.videoFile[0]?.path;
         //const coverImageLocalPath = req.files?.coverImage[0]?.path;
     
+        console.log(req.files)
         let thumbnailLocalPath;
         if (req.files && Array.isArray(req.files.thumbnail) && req.files.thumbnail.length > 0) {
             thumbnailLocalPath = req.files.thumbnail[0].path
@@ -101,7 +100,7 @@ const getVideoById = asyncHandler(async (req, res) => {
 
 
     if (!videoDetails) {
-        throw new ApiError(500, "Something went wrong while retriving the video")
+        throw new ApiError(500, "Video does not exist")
     }
 
     return res.status(201).json(
@@ -112,16 +111,129 @@ const getVideoById = asyncHandler(async (req, res) => {
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
+    const {title, description} = req.body
+    
+    
+    if (!title || !description) {
+            throw new ApiError(400, "All fields are required")
+        }
+
+    const updateFields = {
+        title,
+        description
+    };
+
+    if(req.file !==undefined){
+        const thumbnailLocalPath = req.file.path
+        
+        const thumbnail = await uploadOnCloudinary(thumbnailLocalPath)
+
+        
+        if (!thumbnail) {
+            throw new ApiError(400, "Thumbnail is required")
+        }
+        updateFields.thumbnail = thumbnail.url;
+    }
+
+    
+    const video = await Video.findByIdAndUpdate(
+        new ObjectId(videoId),
+        {
+            $set: updateFields
+        },
+        {new: true}
+        
+    )
+
+    if(!video){
+        throw new ApiError(400, "Something went wrong while updating the video!!")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video details updated successfully"))
 
 })
 
 const deleteVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: delete video
+    if (!videoId?.trim()) {
+        throw new ApiError(400, "Video does not exist")
+    }
+
+    const validId = ObjectId.isValid(videoId) 
+
+    if (!validId) {
+        throw new ApiError(400, "Invalid video ID")
+    }
+    
+    const videoDetails = await Video.findById(new ObjectId(videoId))
+
+    if (!videoDetails) {
+        throw new ApiError(500, "Video does not exist")
+    }
+
+    const result = await Video.deleteOne({ _id: new ObjectId(videoId)})
+
+    if(result.deletedCount==1){
+        console.log(result.deletedCount)
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, deleteVideo, "Video deleted successfully")
+    )
+
+
+    
 })
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params
+    if (!videoId?.trim()) {
+        throw new ApiError(400, "Invalid video ID")
+    }
+
+    if (!ObjectId.isValid(videoId)) {
+        throw new ApiError(400, "Invalid video ID")
+    }
+
+    const currentValue = await Video.findOne(
+        { _id: new ObjectId(videoId) }, // Replace "your_document_id_here" with the actual ID
+        { isPublished: 1 }
+    )
+
+    let msg;
+    let updateFields;
+    
+    if(currentValue.isPublished==true){
+        updateFields ={
+            isPublished : false
+        }
+
+        msg = "Video unpublished"
+    }else{
+        updateFields={
+            isPublished : true
+        }
+
+        msg = "Video Published"
+    }
+
+    const video = await Video.findByIdAndUpdate(
+        new ObjectId(videoId),
+        {
+            $set: updateFields
+        },
+        {new: true}
+        
+    )
+
+    return res.status(201).json(
+        new ApiResponse(200,video, msg)
+    )
+
+
 })
 
 export {
